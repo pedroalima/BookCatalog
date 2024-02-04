876b95bd798b10e4f5f30f9fbeb7b250ad1cffb8
Pedro Lima <tsc.pedrolima@gmail.com>
2024-02-04 10:02:02 -0300

    first commit

---
+++ BookCatalog.Web/Data/BookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BookCatalog.Web.Data
+{
+    public class BookDbContext: DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookCatalog.Web/Models/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Book
+{
+	public Book()
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+		public string Author { get; set; }
+
+		[Range(1, 10, ErrorMessage = "A nota deve ser de 1 a 10")]
+		public int Rating { get; set; }
+
+	}
+}
